8c21fb11bf140b401243a590f1379dc6b07e1015
pitajueshi <549271121@qq.com>
2018-09-13 00:00:52 +0800

    1

---
+++ src/main/java/com/example/demo/JibaApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibaApplication.class, args);
+	}
+}
